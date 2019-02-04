439f41007ad02b79cde1709f0d5d9e9eedc9c0f6
monicamun <mymunoznava@gmail.com>
2019-02-04 10:40:04 -0600

    crear las estructuras de las pantallas en html

--- src/cipher.js
+++ src/cipher.js
@@ -1,26 +1,26 @@
 window.cipher = {
-  encode: function(message, key) {
+  encode: function(key, message) {
     var n = key + 26;
     var result = "";
-    
+
     for (let i = 0; i !== message.length; i++) {
       var c = message.charCodeAt(i);
 
-      c = ((c - 65 + n) % 26) + 65;
-      result += String.fromCharCode(c);
+      let valor = ((c - 65 + n) % 26) + 65;
+      result += String.fromCharCode(valor);
     }
     return result;
   },
 
-  decode: function(message, key) {
+  decode: function(key, message) {
     var n = key + 26;
     var result = "";
 
     for (let i = 0; i !== message.length; i++) {
       var c = message.charCodeAt(i);
 
-      c = ((c - n + 65) % 26) + 65;
-      result += String.fromCharCode(c);
+      valor = ((c - n + 65) % 26) + 65;
+      result += String.fromCharCode(valor);
     }
     return result;
   }
--- src/index.js
+++ src/index.js
@@ -1,23 +1,41 @@
 document.getElementById("btn-encriptar").addEventListener("click", encriptar);
 document.getElementById("btn-desencriptar").addEventListener("click", desencriptar);
+document.getElementById("friend").addEventListener("click",mostarSegundaPantalla );
+document.getElementById("family").addEventListener("click",mostarSegundaPantalla);
+document.getElementById("cople").addEventListener("click",mostarSegundaPantalla );
+document.getElementById("partner").addEventListener("click", mostarSegundaPantalla);
 
+
+
+//valores y variables para encriptar mensaje
 function encriptar() {
   var message = document.getElementById("text").value.toUpperCase();
 
   var key = Number(document.getElementById("key").value);
   var output = document.getElementById("output");
 
-  var result = window.cipher.encode(message, key);
+  var result = cipher.encode(key, message);
 
   output.value = result;
 }
 
+
+//valores y variables para desencriptar
 function desencriptar() {
   var message = document.getElementById("text").value.toUpperCase();
 
   var key = Number(document.getElementById("key").value);
   var output = document.getElementById("output");
 
-  var result = window.cipher.decode(message, key);
+  var result = cipher.decode(key, message);
   output.value = result;
+}
+
+
+//funcion para mostrar una segunda seccion
+function mostarSegundaPantalla() {
+    var secondEscreen = document.getElementById("second-screen")
+    secondEscreen.hidden = false
+    
+
 }
